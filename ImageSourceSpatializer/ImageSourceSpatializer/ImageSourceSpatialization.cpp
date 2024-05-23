da4aad3c67666708d57810d9059d38d683906b06
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-05-22 20:30:19 -0400

    Initial implementation for custom spatializer

---
+++ ImageSourceSpatializer/ImageSourceSpatializer/ImageSourceSpatialization.cpp
@@ -0,0 +1,94 @@
+#include "pch.h"
+#include "AudioPluginUtil.h"
+
+namespace ImageSourceSpatializer
+{
+    enum Parameters
+    {
+        CANHEAR,
+        P_NUMBER
+    };
+
+    struct EffectData
+    {
+        float parameters[P_NUMBER];
+    };
+
+	int InternalRegisterEffectDefinition(UnityAudioEffectDefinition& definition)
+	{
+        int numparams = P_NUMBER;
+        definition.paramdefs = new UnityAudioParameterDefinition[numparams];
+        definition.flags |= UnityAudioEffectDefinitionFlags_IsSpatializer;
+        return numparams;
+	}
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK CreateCallback(UnityAudioEffectState* state)
+    {
+        EffectData* effectdata = new EffectData;
+        memset(effectdata, 0, sizeof(EffectData));
+        state->effectdata = effectdata;
+        //if (IsHostCompatible(state))
+            //state->spatializerdata->distanceattenuationcallback = DistanceAttenuationCallback;
+        AudioPluginUtil::InitParametersFromDefinitions(InternalRegisterEffectDefinition, effectdata->parameters);
+        return UNITY_AUDIODSP_OK;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ReleaseCallback(UnityAudioEffectState* state)
+    {
+        EffectData* data = state->GetEffectData<EffectData>();
+        delete data;
+        return UNITY_AUDIODSP_OK;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK SetFloatParameterCallback(UnityAudioEffectState* state, int index, float value)
+    {
+        EffectData* data = state->GetEffectData<EffectData>();
+        if (index >= P_NUMBER)
+            return UNITY_AUDIODSP_ERR_UNSUPPORTED;
+        data->parameters[index] = value;
+        return UNITY_AUDIODSP_OK;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK GetFloatParameterCallback(UnityAudioEffectState* state, int index, float* value, char* valuestr)
+    {
+        EffectData* data = state->GetEffectData<EffectData>();
+        if (index >= P_NUMBER)
+            return UNITY_AUDIODSP_ERR_UNSUPPORTED;
+        if (value != NULL)
+            *value = data->parameters[index];
+        if (valuestr != NULL)
+            valuestr[0] = 0;
+        return UNITY_AUDIODSP_OK;
+    }
+
+    int UNITY_AUDIODSP_CALLBACK GetFloatBufferCallback(UnityAudioEffectState* state, const char* name, float* buffer, int numsamples)
+    {
+        return UNITY_AUDIODSP_OK;
+    }
+
+    UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ProcessCallback(
+        UnityAudioEffectState* state, float* inbuffer, float* outbuffer, unsigned int length, int inchannels, int outchannels)
+    {
+        // Check that I/O formats are right and that the host API supports this feature
+        /*if (inchannels != 2 || outchannels != 2 ||
+            !IsHostCompatible(state) || state->spatializerdata == NULL)
+        {
+            memcpy(outbuffer, inbuffer, length * outchannels * sizeof(float));
+            return UNITY_AUDIODSP_OK;
+        }*/
+
+        EffectData* data = state->GetEffectData<EffectData>();
+
+        for (unsigned int n = 0; n < length; n++)
+        {
+            for (int i = 0; i < outchannels; i++)
+            {
+                outbuffer[n * outchannels + i] =
+                    inbuffer[n * outchannels + i] *
+                    data->parameters[CANHEAR];
+            }
+        }
+
+        return UNITY_AUDIODSP_OK;
+    }
+}
