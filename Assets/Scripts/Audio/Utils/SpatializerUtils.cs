4863496cd3fc9157dd896631a72a16f551d6d20c
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-06-22 04:15:20 -0400

    Add class for storing collision mesh

--- Assets/Scripts/Audio/Utils/SpatializerUtils.cs
+++ Assets/Scripts/Audio/Utils/SpatializerUtils.cs
@@ -7,6 +7,21 @@ namespace Audio.Utils
         VOLUME = 0
     }
 
+    public class ColliderFace
+    {
+        private Plane _plane;
+        private Vector3[] _vertices = new Vector3[3];
+
+        public Plane GetPlane => _plane;
+        public Vector3[] GetVertices => _vertices;
+
+        public ColliderFace(Plane plane, Vector3[] vertices)
+        {
+            _plane = plane;
+            _vertices = vertices;
+        }
+    }
+
     public static class SpatializerUtils
     {
         public static bool SetSpatializerFloat(AudioSource audioSource, SpatializerIndex index, float value)
