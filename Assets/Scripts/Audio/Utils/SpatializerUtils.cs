02d9f82cd03e5dc9ed73da876ecd2176c4ea7f9f
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-06-22 19:13:04 -0400

    Add GetMeshColliderFaces

--- Assets/Scripts/Audio/Utils/SpatializerUtils.cs
+++ Assets/Scripts/Audio/Utils/SpatializerUtils.cs
@@ -33,5 +33,55 @@ namespace Audio.Utils
         {
             return audioSource.GetSpatializerFloat((int)index, out value);
         }
+
+        public static ColliderFace[] GetColliderFaces(Collider collider)
+        {
+            if (collider == null) { return null; }
+
+            ColliderFace[] colliderFaces = null;
+
+            switch (collider)
+            {
+                case BoxCollider:
+                    colliderFaces = GetBoxColliderFaces(collider);
+                    break;
+                case SphereCollider:
+                    colliderFaces = GetSphereColliderFaces(collider);
+                    break;
+                case MeshCollider:
+                    colliderFaces = GetMeshColliderFaces(collider);
+                    break;
+            }
+
+            return colliderFaces;
+        }
+
+        private static ColliderFace[] GetBoxColliderFaces(Collider collider)
+        {
+            return null;
+        }
+
+        private static ColliderFace[] GetSphereColliderFaces(Collider collider)
+        {
+            return null;
+        }
+
+        private static ColliderFace[] GetMeshColliderFaces(Collider collider)
+        {
+            MeshCollider meshCollider = (MeshCollider)collider;
+            Mesh sharedMesh = meshCollider.sharedMesh;
+            Vector3[] sharedVertices = sharedMesh.vertices;
+            ColliderFace[] colliderFaces = new ColliderFace[sharedVertices.Length / 3];
+
+            for(int i = 0; i < sharedVertices.Length; i += 3)
+            {
+                Vector3[] vertices = { sharedVertices[i], sharedVertices[i + 1], sharedVertices[i + 2] };
+                Plane plane = new Plane(vertices[0], vertices[1], vertices[2]);
+                ColliderFace colliderFace = new (plane, vertices);
+                colliderFaces[i / 3] = colliderFace;
+            }
+
+            return colliderFaces;
+        }
     }
 }
